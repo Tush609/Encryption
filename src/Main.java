13d052b92668e20be8df0c8d25729fcf063cef3b
Tush609 <83871164+Tush609@users.noreply.github.com>
2021-06-28 13:46:18 +0530

    Starting Palindrome

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,10 @@
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Menu m = new Menu();
+        new Menu();
 
     }
 }
--- src/Newfile.java
+++ src/Newfile.java
@@ -1,6 +1,5 @@
 import java.awt.*;
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Scanner;
 
